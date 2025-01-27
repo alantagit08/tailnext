5795cda3a149706a882d01c489c6a023b2ab86c0
alantagit08 <125828477+alantagit08@users.noreply.github.com>
2025-01-27 12:52:53 -0500

    Initial set up

--- src/shared/data/pages/home.data.tsx
+++ src/shared/data/pages/home.data.tsx
@@ -41,8 +41,7 @@ import gasImg from '~/assets/images/gas.jpg';
 export const heroHome: HeroProps = {
   title: (
     <>
-      Free template for <span className="hidden md:inline">starts a website using</span> <span>Next.js</span> +{' '}
-      <span className="sm:whitespace-nowrap">Tailwind CSS</span>
+     Engineers Recruiting Engineers: Bridging the Gap 
     </>
   ),
   subtitle: (
